85cdf44e5c837f7af7094106aa6e3c021f2f5d8c
TusharAm9 <tusharbarmatfg@gmail.com>
2025-01-29 12:23:44 +0530

    route changes

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-if(process.env !="production"){
-    require('dotenv').config();
+if (process.env != "production") {
+  require("dotenv").config();
 }
 
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose");
-const path=require("path");
-const methodOverride=require("method-override");
-const ejsMate=require("ejs-mate");
-const ExpressError=require("./utils/ExpressError.js");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User=require("./models/user.js");
+const User = require("./models/user.js");
 
-const dbUrl=process.env.ATLUS_DB;
+const dbUrl = process.env.ATLUS_DB;
 
 async function main() {
-    await mongoose.connect(dbUrl);
+  await mongoose.connect(dbUrl);
 }
 
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto:{
-        secret:process.env.SECRET,
-    },
-    touchAfter:24*3600,
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
 });
 
-store.on("error",()=>{
-    console.log("Error in Mongo session",err);
+store.on("error", () => {
+  console.log("Error in Mongo session", err);
 });
 
 const SessionProperties = {
-    store,
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie:{
-        expires: Date.now() + 3 * 24 * 60 * 60 * 1000,
-        maxAge : 3 * 24 * 60 * 60 * 1000,
-        httpOnly:true,
-    }
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 3 * 24 * 60 * 60 * 1000,
+    maxAge: 3 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 app.use(session(SessionProperties));
 app.use(flash());
@@ -56,45 +56,46 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
 });
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs",ejsMate);
-app.use(express.static(path.join(__dirname,"public")));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 const listingsRoute = require("./routes/listings.js");
 const reviewsRoute = require("./routes/reviews.js");
 const userRoute = require("./routes/user.js");
 
-
-main().then(()=>{
+main()
+  .then(() => {
     console.log("connected to DB");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-app.use("/listings",listingsRoute);
-app.use("/listings/:id/reviews",reviewsRoute);
-app.use("/",userRoute);
+app.use("/", listingsRoute);
+app.use("/user", userRoute);
+app.use("/listings/:id/reviews", reviewsRoute);
 
 //error handleing middleware
-app.all("*",(req,res,next)=>{
-    next(new ExpressError(404, "Page Not Found!"));
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found!"));
+});
 
 app.use((err, req, res, next) => {
-    let{statusCode=500,message="Something went Wrong!"}=err; 
-    res.status(statusCode).render("listing/error.ejs",{message});
-})
+  let { statusCode = 500, message = "Something went Wrong!" } = err;
+  res.status(statusCode).render("listing/error.ejs", { message });
+});
 
-app.listen(8080,()=>{
-    console.log("app is listening on post 8080");
-})
+app.listen(8080, () => {
+  console.log("app is listening on post 8080");
+});
